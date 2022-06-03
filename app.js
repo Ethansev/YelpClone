cc9b435faee9f5bcf0db2bd6f42e7ed25ff4d79d
Ethansev <sevadus98@gmail.com>
2022-06-03 08:16:31 -0700

    Installed package 'express-mongo-sanitize' to fight against SQL (mongo?) injections

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-santize');
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
@@ -39,6 +40,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded( {extended:true} ));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public'))); //serves the public directory
+app.use(mongoSanitize());
 
 const sessionConfig = {
     secret:'thisshouldbeabettersecret',
